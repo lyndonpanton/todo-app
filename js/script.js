87155de068d5904533174ae2beab1b8e1a3dc153
Lyndon Panton <LyndonPanton@gmail.com>
2025-02-18 00:27:21 +0000

    Todo list items can be added to the todo list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     let todoForm = document.getElementById("todo-form");
-    todoForm.addEventListener("submit", function(event) {
+    todoForm.addEventListener("submit", handleTodoFormSubmission);
+    
+    function handleTodoFormSubmission(event) {
         event.preventDefault();
-    })
+
+        let todoList = document.getElementById("todo-list");
+        let todoFormName = document.getElementById("todo-form-name");
+        let todoFormCategory = document.getElementById("todo-form-category");
+
+        console.log(todoList.getAttribute("id"));
+        console.log(todoFormName.getAttribute("placeholder"));
+        console.log(todoFormCategory.getAttribute("value"));
+
+        /*
+            Later, each category should have its own todo list, and you should
+            add the todo to the relevant todo list
+        */
+        let todo = document.createElement("li");
+        let todoText = document.createTextNode(
+            todoFormName.value
+        );
+
+        todo.classList.add("todo-list-item");
+        todo.appendChild(todoText);
+
+        todoList.appendChild(todo);
+    }
 });
