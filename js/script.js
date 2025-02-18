8bb0e06f899ef6ed713f3ba8a032cb7998582c32
Lyndon Panton <LyndonPanton@gmail.com>
2025-02-18 00:14:19 +0000

    Page no longer reloads on form submission

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    let todoForm = document.getElementById("todo-form");
+    todoForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+    })
 });
